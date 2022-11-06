4e863f9ae7554e78d157b1f06f9be52d362bf65d
Huzaifa-Khalid-2 <Huzaifa-Khalid-2@users.noreply.github.com>
2022-11-06 12:45:40 +0000

    Prettified Code!

--- src/Jet.ts
+++ src/Jet.ts
@@ -5,19 +5,19 @@
  * Since:   2022-10-21
  */
 
- import Airplane from './Airplane'
+import Airplane from "./Airplane";
 
- class Jet extends Airplane {
-   // Speed
-   private readonly multiplier = 2
- 
-   setSpeed(speed: number): void {
-     super.setSpeed(speed * this.multiplier)
-   }
- 
-   accelerate(): void {
-     super.setSpeed(super.getSpeed() * 2)
-   }
- }
- 
- export = Jet
+class Jet extends Airplane {
+  // Speed
+  private readonly multiplier = 2;
+
+  setSpeed(speed: number): void {
+    super.setSpeed(speed * this.multiplier);
+  }
+
+  accelerate(): void {
+    super.setSpeed(super.getSpeed() * 2);
+  }
+}
+
+export = Jet;
